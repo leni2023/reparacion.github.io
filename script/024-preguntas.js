60e8eb1c60bd206b190e3cbbff61050801f17b36
leni2023 <llasadavid123147@gmail.com>
2023-06-06 13:21:40 -0500

    terceroSubido

---
+++ script/024-preguntas.js
@@ -0,0 +1,292 @@
+{
+    const botona1 = document.querySelector(".a0011-024");
+    const boton2a1 = document.querySelector(".a0011-0241");
+    const amplioa1 = document.querySelector(".a0012-024");
+
+    const botona2 = document.querySelector(".a0011-024a20");
+    const boton2a2 = document.querySelector(".a0011-024a21");
+    const amplioa2 = document.querySelector(".a0012-024a22");
+
+    const botona3 = document.querySelector(".a0011-024a30");
+    const boton2a3 = document.querySelector(".a0011-024a31");
+    const amplioa3 = document.querySelector(".a0012-024a32");
+
+    const botona4 = document.querySelector(".a0011-024a40");
+    const boton2a4 = document.querySelector(".a0011-024a41");
+    const amplioa4 = document.querySelector(".a0012-024a42");
+
+    const botona5 = document.querySelector(".a0011-024a50");
+    const boton2a5 = document.querySelector(".a0011-024a51");
+    const amplioa5 = document.querySelector(".a0012-024a52");
+
+    const botona6 = document.querySelector(".a0011-024a60");
+    const boton2a6 = document.querySelector(".a0011-024a61");
+    const amplioa6 = document.querySelector(".a0012-024a62");
+
+    const botona7 = document.querySelector(".a0011-024a70");
+    const boton2a7 = document.querySelector(".a0011-024a71");
+    const amplioa7 = document.querySelector(".a0012-024a72");
+
+    const botona8 = document.querySelector(".a0011-024a80");
+    const boton2a8 = document.querySelector(".a0011-024a81");
+    const amplioa8 = document.querySelector(".a0012-024a82");
+
+    const botona9 = document.querySelector(".a0011-024a90");
+    const boton2a9 = document.querySelector(".a0011-024a91");
+    const amplioa9 = document.querySelector(".a0012-024a92");
+
+    const botona10 = document.querySelector(".a0011-024a100");
+    const boton2a10 = document.querySelector(".a0011-024a101");
+    const amplioa10 = document.querySelector(".a0012-024a102");
+
+    const botona11 = document.querySelector(".a0011-024a110");
+    const boton2a11 = document.querySelector(".a0011-024a111");
+    const amplioa11 = document.querySelector(".a0012-024a112");
+
+    const botona12 = document.querySelector(".a0011-024a120");
+    const boton2a12 = document.querySelector(".a0011-024a121");
+    const amplioa12 = document.querySelector(".a0012-024a122");
+
+    const botona13 = document.querySelector(".a0011-024a130");
+    const boton2a13 = document.querySelector(".a0011-024a131");
+    const amplioa13 = document.querySelector(".a0012-024a132");
+
+    const botona14 = document.querySelector(".a0011-024a140");
+    const boton2a14 = document.querySelector(".a0011-024a141");
+    const amplioa14 = document.querySelector(".a0012-024a142");
+
+    const botona15 = document.querySelector(".a0011-024a150");
+    const boton2a15 = document.querySelector(".a0011-024a151");
+    const amplioa15 = document.querySelector(".a0012-024a152");
+
+    const botona16 = document.querySelector(".a0011-024a160");
+    const boton2a16 = document.querySelector(".a0011-024a161");
+    const amplioa16 = document.querySelector(".a0012-024a162");
+
+    const botona17 = document.querySelector(".a0011-024a170");
+    const boton2a17 = document.querySelector(".a0011-024a171");
+    const amplioa17 = document.querySelector(".a0012-024a172");
+
+    const botona18 = document.querySelector(".a0011-024a180");
+    const boton2a18 = document.querySelector(".a0011-024a181");
+    const amplioa18 = document.querySelector(".a0012-024a182");
+
+
+    botona1.addEventListener('click', () =>{
+        amplioa1.classList.remove('a00121-024--1');
+        botona1.classList.add('a0011-024--01');
+        boton2a1.classList.remove('a0011-024--01');
+    });
+    boton2a1.addEventListener('click', () =>{
+        amplioa1.classList.add('a00121-024--1');
+        botona1.classList.remove('a0011-024--01');
+        boton2a1.classList.add('a0011-024--01');
+    });
+
+    // 2222222222222222222222222222
+    botona2.addEventListener('click', () =>{
+        amplioa2.classList.remove('a00121-024--1');
+        botona2.classList.add('a0011-024--01');
+        boton2a2.classList.remove('a0011-024--01');
+    });
+    boton2a2.addEventListener('click', () =>{
+        amplioa2.classList.add('a00121-024--1');
+        botona2.classList.remove('a0011-024--01');
+        boton2a2.classList.add('a0011-024--01');
+    });
+
+    // 33333333333333333333333333333
+    botona3.addEventListener('click', () =>{
+        amplioa3.classList.remove('a00121-024--1');
+        botona3.classList.add('a0011-024--01');
+        boton2a3.classList.remove('a0011-024--01');
+    });
+    boton2a3.addEventListener('click', () =>{
+        amplioa3.classList.add('a00121-024--1');
+        botona3.classList.remove('a0011-024--01');
+        boton2a3.classList.add('a0011-024--01');
+    });
+
+    // 44444444444444444444444444444
+    botona4.addEventListener('click', () =>{
+        amplioa4.classList.remove('a00121-024--1');
+        botona4.classList.add('a0011-024--01');
+        boton2a4.classList.remove('a0011-024--01');
+    });
+    boton2a4.addEventListener('click', () =>{
+        amplioa4.classList.add('a00121-024--1');
+        botona4.classList.remove('a0011-024--01');
+        boton2a4.classList.add('a0011-024--01');
+    });
+
+    // 55555555555555555555555555555
+    botona5.addEventListener('click', () =>{
+        amplioa5.classList.remove('a00121-024--1');
+        botona5.classList.add('a0011-024--01');
+        boton2a5.classList.remove('a0011-024--01');
+    });
+    boton2a5.addEventListener('click', () =>{
+        amplioa5.classList.add('a00121-024--1');
+        botona5.classList.remove('a0011-024--01');
+        boton2a5.classList.add('a0011-024--01');
+    });
+
+    // 666666666666666666666666
+    botona6.addEventListener('click', () =>{
+        amplioa6.classList.remove('a00121-024--1');
+        botona6.classList.add('a0011-024--01');
+        boton2a6.classList.remove('a0011-024--01');
+    });
+    boton2a6.addEventListener('click', () =>{
+        amplioa6.classList.add('a00121-024--1');
+        botona6.classList.remove('a0011-024--01');
+        boton2a6.classList.add('a0011-024--01');
+    });
+
+    // 7777777777777777777777
+    botona7.addEventListener('click', () =>{
+        amplioa7.classList.remove('a00121-024--1');
+        botona7.classList.add('a0011-024--01');
+        boton2a7.classList.remove('a0011-024--01');
+    });
+    boton2a7.addEventListener('click', () =>{
+        amplioa7.classList.add('a00121-024--1');
+        botona7.classList.remove('a0011-024--01');
+        boton2a7.classList.add('a0011-024--01');
+    });
+
+    // 8888888888888888888888888888
+    botona8.addEventListener('click', () =>{
+        amplioa8.classList.remove('a00121-024--1');
+        botona8.classList.add('a0011-024--01');
+        boton2a8.classList.remove('a0011-024--01');
+    });
+    boton2a8.addEventListener('click', () =>{
+        amplioa8.classList.add('a00121-024--1');
+        botona8.classList.remove('a0011-024--01');
+        boton2a8.classList.add('a0011-024--01');
+    });
+
+    // 99999999999999999999999999999
+    botona9.addEventListener('click', () =>{
+        amplioa9.classList.remove('a00121-024--1');
+        botona9.classList.add('a0011-024--01');
+        boton2a9.classList.remove('a0011-024--01');
+    });
+    boton2a9.addEventListener('click', () =>{
+        amplioa9.classList.add('a00121-024--1');
+        botona9.classList.remove('a0011-024--01');
+        boton2a9.classList.add('a0011-024--01');
+    });
+
+    // 101010101010101010101010
+    botona10.addEventListener('click', () =>{
+        amplioa10.classList.remove('a00121-024--1');
+        botona10.classList.add('a0011-024--01');
+        boton2a10.classList.remove('a0011-024--01');
+    });
+    boton2a10.addEventListener('click', () =>{
+        amplioa10.classList.add('a00121-024--1');
+        botona10.classList.remove('a0011-024--01');
+        boton2a10.classList.add('a0011-024--01');
+    });
+
+    // 11 11 11 11 11 11 11 11 11 
+    botona11.addEventListener('click', () =>{
+        amplioa11.classList.remove('a00121-024--1');
+        botona11.classList.add('a0011-024--01');
+        boton2a11.classList.remove('a0011-024--01');
+    });
+    boton2a11.addEventListener('click', () =>{
+        amplioa11.classList.add('a00121-024--1');
+        botona11.classList.remove('a0011-024--01');
+        boton2a11.classList.add('a0011-024--01');
+    });
+
+    // 121212121212121212121212121212
+    botona12.addEventListener('click', () =>{
+        amplioa12.classList.remove('a00121-024--1');
+        botona12.classList.add('a0011-024--01');
+        boton2a12.classList.remove('a0011-024--01');
+    });
+    boton2a12.addEventListener('click', () =>{
+        amplioa12.classList.add('a00121-024--1');
+        botona12.classList.remove('a0011-024--01');
+        boton2a12.classList.add('a0011-024--01');
+    });
+
+    // 13131313131313131313131313
+    botona13.addEventListener('click', () =>{
+        amplioa13.classList.remove('a00121-024--1');
+        botona13.classList.add('a0011-024--01');
+        boton2a13.classList.remove('a0011-024--01');
+    });
+    boton2a13.addEventListener('click', () =>{
+        amplioa13.classList.add('a00121-024--1');
+        botona13.classList.remove('a0011-024--01');
+        boton2a13.classList.add('a0011-024--01');
+    });
+
+    // 141414141414141414141414
+    botona14.addEventListener('click', () =>{
+        amplioa14.classList.remove('a00121-024--1');
+        botona14.classList.add('a0011-024--01');
+        boton2a14.classList.remove('a0011-024--01');
+    });
+    boton2a14.addEventListener('click', () =>{
+        amplioa14.classList.add('a00121-024--1');
+        botona14.classList.remove('a0011-024--01');
+        boton2a14.classList.add('a0011-024--01');
+    });
+
+    // 1515151515151515151515
+    botona15.addEventListener('click', () =>{
+        amplioa15.classList.remove('a00121-024--1');
+        botona15.classList.add('a0011-024--01');
+        boton2a15.classList.remove('a0011-024--01');
+    });
+    boton2a15.addEventListener('click', () =>{
+        amplioa15.classList.add('a00121-024--1');
+        botona15.classList.remove('a0011-024--01');
+        boton2a15.classList.add('a0011-024--01');
+    });
+
+    // 16516161616161616161616161616
+    botona16.addEventListener('click', () =>{
+        amplioa16.classList.remove('a00121-024--1');
+        botona16.classList.add('a0011-024--01');
+        boton2a16.classList.remove('a0011-024--01');
+    });
+    boton2a16.addEventListener('click', () =>{
+        amplioa16.classList.add('a00121-024--1');
+        botona16.classList.remove('a0011-024--01');
+        boton2a16.classList.add('a0011-024--01');
+    });
+
+    // 171717171717171717171717
+    botona17.addEventListener('click', () =>{
+        amplioa17.classList.remove('a00121-024--1');
+        botona17.classList.add('a0011-024--01');
+        boton2a17.classList.remove('a0011-024--01');
+    });
+    boton2a17.addEventListener('click', () =>{
+        amplioa17.classList.add('a00121-024--1');
+        botona17.classList.remove('a0011-024--01');
+        boton2a17.classList.add('a0011-024--01');
+    });
+
+    // 181818181818181818181818
+    botona18.addEventListener('click', () =>{
+        amplioa18.classList.remove('a00121-024--1');
+        botona18.classList.add('a0011-024--01');
+        boton2a18.classList.remove('a0011-024--01');
+    });
+    boton2a18.addEventListener('click', () =>{
+        amplioa18.classList.add('a00121-024--1');
+        botona18.classList.remove('a0011-024--01');
+        boton2a18.classList.add('a0011-024--01');
+    });
+}
+
+
+
