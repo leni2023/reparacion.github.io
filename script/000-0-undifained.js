23bdcaafc40ec4652436f422f65dfa223664736a
leni2023 <llasadavid123147@gmail.com>
2023-06-19 14:13:42 -0500

    estoy en peliculas

---
+++ script/000-0-undifained.js
@@ -0,0 +1,196 @@
+// Este es para el apartado de aprende mas
+{
+    const aparecer = document.querySelector('.C-042-13');
+    const abrir = document.querySelector('.C-043');
+    const cerrar = document.querySelector('.C-046');
+    const video = document.querySelector('.C-044-22');
+
+    aparecer.addEventListener('click', () =>{
+        abrir.classList.remove('C-043none');
+        video.classList.remove('C-044-22none');
+    });
+    cerrar.addEventListener('click', () =>{
+        abrir.classList.add('C-043none');
+        video.classList.add('C-044-22none');
+        alert('De no ponerle en pausa se seguirá escuhando el sonido del video al salir');
+    });
+}
+// para el video 2
+{
+    const aparecer = document.querySelector('.C-042-13player2');
+    const abrir = document.querySelector('.C-043video2');
+    const cerrar = document.querySelector('.C-046cerrar2');
+    const video = document.querySelector('.C-044-22');
+
+    aparecer.addEventListener('click', () =>{
+        abrir.classList.remove('C-043none');
+        video.classList.remove('C-044-22none');
+    });
+    cerrar.addEventListener('click', () =>{
+        abrir.classList.add('C-043none');
+        video.classList.add('C-044-22none');
+        alert('De no ponerle en pausa se seguirá escuhando el sonido del video al salir');
+    });
+}
+// para el video 3
+{
+    const aparecer = document.querySelector('.C-042-13player3');
+    const abrir = document.querySelector('.C-043video3');
+    const cerrar = document.querySelector('.C-046cerrar3');
+    const video = document.querySelector('.C-044-22');
+
+    aparecer.addEventListener('click', () =>{
+        abrir.classList.remove('C-043none');
+        video.classList.remove('C-044-22none');
+    });
+    cerrar.addEventListener('click', () =>{
+        abrir.classList.add('C-043none');
+        video.classList.add('C-044-22none');
+        alert('De no ponerle en pausa se seguirá escuhando el sonido del video al salir');
+    });
+}
+// para el video 4
+{
+    const aparecer = document.querySelector('.C-042-13player4');
+    const abrir = document.querySelector('.C-043video4');
+    const cerrar = document.querySelector('.C-046cerrar4');
+    const video = document.querySelector('.C-044-22');
+
+    aparecer.addEventListener('click', () =>{
+        abrir.classList.remove('C-043none');
+        video.classList.remove('C-044-22none');
+    });
+    cerrar.addEventListener('click', () =>{
+        abrir.classList.add('C-043none');
+        video.classList.add('C-044-22none');
+        alert('De no ponerle en pausa se seguirá escuhando el sonido del video al salir');
+    });
+}
+// para el video 5
+{
+    const aparecer = document.querySelector('.C-042-13player5');
+    const abrir = document.querySelector('.C-043video5');
+    const cerrar = document.querySelector('.C-046cerrar5');
+    const video = document.querySelector('.C-044-22');
+
+    aparecer.addEventListener('click', () =>{
+        abrir.classList.remove('C-043none');
+        video.classList.remove('C-044-22none');
+    });
+    cerrar.addEventListener('click', () =>{
+        abrir.classList.add('C-043none');
+        video.classList.add('C-044-22none');
+        alert('De no ponerle en pausa se seguirá escuhando el sonido del video al salir');
+    });
+}
+// para el video 6
+{
+    const aparecer = document.querySelector('.C-042-13player6');
+    const abrir = document.querySelector('.C-043video6');
+    const cerrar = document.querySelector('.C-046cerrar6');
+    const video = document.querySelector('.C-044-22');
+
+    aparecer.addEventListener('click', () =>{
+        abrir.classList.remove('C-043none');
+        video.classList.remove('C-044-22none');
+    });
+    cerrar.addEventListener('click', () =>{
+        abrir.classList.add('C-043none');
+        video.classList.add('C-044-22none');
+        alert('De no ponerle en pausa se seguirá escuhando el sonido del video al salir');
+    });
+}
+// para el video 7
+{
+    const aparecer = document.querySelector('.C-042-13player7');
+    const abrir = document.querySelector('.C-043video7');
+    const cerrar = document.querySelector('.C-046cerrar7');
+    const video = document.querySelector('.C-044-22');
+
+    aparecer.addEventListener('click', () =>{
+        abrir.classList.remove('C-043none');
+        video.classList.remove('C-044-22none');
+    });
+    cerrar.addEventListener('click', () =>{
+        abrir.classList.add('C-043none');
+        video.classList.add('C-044-22none');
+        alert('De no ponerle en pausa se seguirá escuhando el sonido del video al salir');
+    });
+}
+// para el video 8
+{
+    const aparecer = document.querySelector('.C-042-13player8');
+    const abrir = document.querySelector('.C-043video8');
+    const cerrar = document.querySelector('.C-046cerrar8');
+    const video = document.querySelector('.C-044-22');
+
+    aparecer.addEventListener('click', () =>{
+        abrir.classList.remove('C-043none');
+        video.classList.remove('C-044-22none');
+    });
+    cerrar.addEventListener('click', () =>{
+        abrir.classList.add('C-043none');
+        video.classList.add('C-044-22none');
+        alert('De no ponerle en pausa se seguirá escuhando el sonido del video al salir');
+    });
+}
+// para el video 9
+{
+    const aparecer = document.querySelector('.C-042-13player9');
+    const abrir = document.querySelector('.C-043video9');
+    const cerrar = document.querySelector('.C-046cerrar9');
+    const video = document.querySelector('.C-044-22');
+
+    aparecer.addEventListener('click', () =>{
+        abrir.classList.remove('C-043none');
+        video.classList.remove('C-044-22none');
+    });
+    cerrar.addEventListener('click', () =>{
+        abrir.classList.add('C-043none');
+        video.classList.add('C-044-22none');
+        alert('De no ponerle en pausa se seguirá escuhando el sonido del video al salir');
+    });
+}
+// para el video 10
+{
+    const aparecer = document.querySelector('.C-042-13player10');
+    const abrir = document.querySelector('.C-043video10');
+    const cerrar = document.querySelector('.C-046cerrar10');
+    const video = document.querySelector('.C-044-22');
+
+    aparecer.addEventListener('click', () =>{
+        abrir.classList.remove('C-043none');
+        video.classList.remove('C-044-22none');
+    });
+    cerrar.addEventListener('click', () =>{
+        abrir.classList.add('C-043none');
+        video.classList.add('C-044-22none');
+        alert('De no ponerle en pausa se seguirá escuhando el sonido del video al salir');
+    });
+}
+// para el video 11
+{
+    const aparecer = document.querySelector('.C-042-13player11');
+    const abrir = document.querySelector('.C-043video11');
+    const cerrar = document.querySelector('.C-046cerrar11');
+    const video = document.querySelector('.C-044-22');
+
+    aparecer.addEventListener('click', () =>{
+        abrir.classList.remove('C-043none');
+        video.classList.remove('C-044-22none');
+    });
+    cerrar.addEventListener('click', () =>{
+        abrir.classList.add('C-043none');
+        video.classList.add('C-044-22none');
+        alert('De no ponerle en pausa se seguirá escuhando el sonido del video al salir');
+    });
+}
+
+// {
+//     function mute() {
+//     var aud = document.getElementById("music");
+//         if (aud.muted == false) {aud.muted = true}
+//         else {aud.muted = false}
+//     }
+
+// }
